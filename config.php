d49159365642906704d98ea5519fe6e88870629f
teruto725 <akiteruto@gmail.com>
2019-11-01 18:11:24 +0900

    Translated into Japanese

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//ini_set('display_errors', 1);
-
-define('DSN', 'mysql:host=157.112.147.201;dbname=teruto725_tango');
-define('DB_USER', 'teruto725_d');
-define('DB_PASS', 'teruto725');
-?>
